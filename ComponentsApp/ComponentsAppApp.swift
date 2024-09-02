b95ad94f001990a51dd66aec388d3fc0e37c8beb
shafia <shafiamanzoor762@gmail.com>
2024-09-02 17:50:40 +0500

    Initial Commit

---
+++ ComponentsApp/ComponentsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComponentsAppApp.swift
+//  ComponentsApp
+//
+//  Created by apple on 02/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct ComponentsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
